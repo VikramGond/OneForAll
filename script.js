fa7133f7d28485a5fa79a8657843d231829b70ee
vikramgond <vikramgond22052@gmail.com>
2025-08-24 19:36:35 +0530

    fix the smalll issue of prime error

--- script.js
+++ script.js
@@ -17,32 +17,38 @@ const calcFactorial = () => {
 
 }
 
-
-const isPrime = () => {
-
+function isPrime() {
   let num = parseInt(document.getElementById("pNum").value);
 
-  if(num <= 1){
-    document.getElementById("result2").innerHTML = "Please Enter a Number greater than 1."
+  if (num <= 1) {
+    document.getElementById("result2").innerHTML = "Please Enter a Number greater than 1.";
+    return;
   }
 
-  if(num === 2){
-    document.getElementById("result2").innerHTML = `The ${num} is a Prime Number.`
+  if (num === 2) {
+    document.getElementById("result2").innerHTML = `The ${num} is a Prime Number.`;
+    return;
   }
 
-  if(num % 2 === 0){
-    document.getElementById("result2").innerHTML= `The ${num} is not a Prime Number.`
+  if (num % 2 === 0) {
+    document.getElementById("result2").innerHTML = `The ${num} is not a Prime Number.`;
+    return;
   }
 
-  else{
-    for(let i = 3; i <= Math.sqrt(num);i += 2){
-      if(num % i === 0){
-        document.getElementById("result2").innerHTML = `The ${num} is not a Prime Number.`
-      }else{
-        document.getElementById("result2").innerHTML = `The ${num} is a Prime Number.`
-      }
-
+  let prime = true;
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) {
+      prime = false;
+      break;
     }
   }
+
+  if (prime) {
+    document.getElementById("result2").innerHTML = `The ${num} is a Prime Number.`;
+  } else {
+    document.getElementById("result2").innerHTML = `The ${num} is not a Prime Number.`;
+  }
 }
+
+
 
